5809e9e3b7cd030c89a8f9f260cd303f30605a15
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-07-13 02:25:11 +0530

    Graph

--- Day35.cpp
+++ Day35.cpp
@@ -1,10 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+template<typename T> 
+
 class graph{
-    
+    public:
+        unordered_map<T, list<T>> adj;
+
+        void addEdge(T u, T v, bool direction){
+            // direction =0 ->undirected graph
+            // direction =1 ->directed graph
+
+            // create an edge from u to v
+            adj[u].push_back(v);
+            if(direction==0){
+                adj[v].push_back(u);
+            }
+        }
+
+        void printAdjList(){
+            for(auto i : adj){
+                cout << i.first << "-> ";
+                for(auto j : i.second){
+                    cout << j << ", ";
+                }
+                cout << endl;
+            }
+            cout << endl;
+        }
 };
 
 int main(){
 
+        int n;
+        cout << "Enter the no. of nodes " << endl;
+        cin >> n;
+
+        int m;
+        cout << "Enter the number of edges " << endl;
+        cin >> m;
+
+        graph<char>g;
+
+        for (auto i = 0; i < m;i++){
+            char u, v;
+            cin >> u >> v;
+            g.addEdge(u, v, 0);
+        }
+
+        g.printAdjList();
 }
