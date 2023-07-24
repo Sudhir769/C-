e659acb1d56d9fdf4eb141b79093126e86507cff
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-07-24 22:28:41 +0530

    Doubly LinkedList

--- Day44.cpp
+++ Day44.cpp
@@ -4,41 +4,119 @@ using namespace std;
 class Node{
 public:
     int val;
+    Node *prev;
     Node *next;
 
     Node(int data){
         this->val =data;
+        this->prev = NULL;
         this->next = NULL;
     }
 };
 
-class LinkedList{
+class DoublyLinkedList{
     public:
         Node *head;
+        Node *tail;
 
-        LinkedList(){
+        DoublyLinkedList(){
             head = NULL;
+            tail =  NULL;
         }
 
-        void insertAtTail(int value){
+        void insertAtStart(int value){
             Node* new_node = new Node(value);
             if(head==NULL){
                 head = new_node;
+                tail = new_node;
                 return;
             }
-            Node *temp = head;
-            while(temp->next!=NULL){
-                temp = temp->next;
-            }
-            temp->next = new_node;
+
+            new_node->next = head;
+            head->prev = new_node;
+            head = new_node;
+            return;
         }
+        void insertAtEnd(int val){
+            Node *new_node = new Node(val);
+            if(tail==NULL){
+                head = new_node;
+                tail = new_node;
+                return;
+            }
+            tail->next = new_node;
+            new_node->prev = tail;
+            tail = new_node;
+            return;
+        }
+
         void display(){
             Node *temp = head;
+            cout << "NULL<-";
             while(temp!=NULL){
-                cout << temp->val << "->";
+                cout << temp->val << "<->";
                 temp = temp->next;
             }
             cout << "NULL"<<endl;
+            cout << endl;
+        }
+
+        void insertAtPosition(int val, int k){
+            Node *temp = head;
+            int counter = 1;
+            while(counter<(k-1)){
+                temp = temp->next;
+                counter++;
+            }
+            Node *new_node = new Node(val);
+            new_node->next = temp->next;
+            temp->next = new_node;
+
+            new_node->prev = temp;
+            new_node->next->prev = new_node;
+            return;
+        }
+
+        void deleteAtStart(){
+            if(head==NULL){
+                return;
+            }
+            Node *temp = head;
+            head = head->next;
+            if(head==NULL){
+                tail = NULL;
+            }else{
+                head->prev = NULL;
+            }
+            free(temp);
+            return;
+        }
+
+        void deleteAtEnd(){
+            if(head==NULL){
+                return;
+            }
+
+            Node *temp = tail;
+            tail = tail->prev;
+            if (tail==NULL){
+                head = NULL;
+            }else{
+                tail->next = NULL;
+            }
+            free(temp);
+            return;
+        }
+
+        void deleteAtPosition(int k){
+            Node *temp = head;
+            int counter = 1;
+            while(counter<k){
+                temp = temp->next;
+                counter++;
+            }
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
         }
 };
 
@@ -47,6 +125,32 @@ class LinkedList{
 
 
 int main(){
-        // sortListMain();
-        
+        Node *new_node = new Node(3);
+
+        DoublyLinkedList dll;
+
+        dll.insertAtStart(5);
+        dll.insertAtStart(3);
+        dll.insertAtStart(1);
+
+        dll.insertAtEnd(7);
+        dll.insertAtEnd(9);
+        dll.insertAtEnd(11);
+
+        dll.insertAtPosition(2, 2);
+        dll.insertAtPosition(4, 4);
+        dll.insertAtPosition(6, 6);
+        dll.insertAtPosition(8, 8);
+        dll.insertAtPosition(10, 10);
+
+        dll.display();
+
+        dll.deleteAtStart();
+        dll.display();
+
+        dll.deleteAtEnd();
+        dll.display();
+
+        dll.deleteAtPosition(5);
+        dll.display();
 }
