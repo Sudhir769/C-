1e6ea5400a5053247acc92eefd713eab50dab979
Sudhir Maurya <98456936+Sudhir769@users.noreply.github.com>
2023-07-25 20:59:53 +0530

    CircularLL & STL lists & template class

--- Day46.cpp
+++ Day46.cpp
@@ -99,6 +99,73 @@ class node{
         }
 };
 
+void listSTL(){
+    list<int> l1 = {1, 3, 5, 7, 9};
+    list<int> l2 = {62, 74, 83, 59, 42};
+    auto itr1 = l1.begin();
+    auto itr2 = l1.end();
+    cout << *itr1 << " " << *itr2 << endl;
+
+    auto itr3= l1.rbegin();
+    auto itr4 = l1.rend();
+    cout << *itr3 << " " << *itr4 << endl;
+
+    advance(itr1, 2);
+    cout << *itr1 << endl;
+
+    for(auto num: l1){
+        cout << num << " ";
+    }
+    cout << endl;
+
+    for (auto itr = l2.begin(); itr != l2.end();itr++){
+        cout << *itr << " ";
+    }
+    cout << endl;
+
+    for (auto itr = l2.rbegin(); itr != l2.rend();itr++){
+        cout << *itr << " ";
+    }
+    cout << endl;
+
+    itr1 = l1.begin();
+    itr1 = l1.begin();
+
+    advance(itr1, 1);
+    l1.insert(itr1, 2);
+    l1.insert(itr1, 2, 2);
+    l1.insert(itr1, itr1,itr2);
+
+    for(auto num: l1){
+        cout << num << " ";
+    }
+    cout << endl;
+
+    // l1.erase(itr1);
+
+    l1.erase(itr1, itr2);
+    for(auto num: l1){
+        cout << num << " ";
+    }
+    cout << endl;
+
+    l1.push_front(0);
+    l1.push_back(11);
+    
+    for(auto num: l1){
+        cout << num << " ";
+    }
+    cout << endl;
+
+    l1.pop_front();
+    l1.pop_back();
+    
+    for(auto num: l1){
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
 int main(){
     CircularLinkedList cll;
     cll.insertAtStart(3);
@@ -115,4 +182,6 @@ int main(){
     node<string> *node2 = new node<string>("Sudhir");
     cout << node1->val<<endl;
     cout << node2->val<<endl;
+
+    listSTL();
 }
--- Day47.cpp
+++ Day47.cpp
@@ -1,15 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<typename T>
+int main(){
 
-class node{
-    public:
-        T val;
-        node *next;
-
-        node(T data){
-            val = data;
-            next = NULL;
-        }
 }
